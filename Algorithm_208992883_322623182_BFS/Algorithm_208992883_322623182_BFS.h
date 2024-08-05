6f8fb8d5fe0ce21b46f7631c116fb3d304a3dba9
Ev1a23 <89100342+Ev1a23@users.noreply.github.com>
2024-08-05 13:01:14 +0000

    Algorithm supposed to work

--- Algorithm_208992883_322623182_BFS/Algorithm_208992883_322623182_BFS.h
+++ Algorithm_208992883_322623182_BFS/Algorithm_208992883_322623182_BFS.h
@@ -4,22 +4,27 @@
 #include "../algorithm/MyAlgorithm.h"
 #include "../algorithm/AlgorithmRegistration.h"
 #include <queue>
-#include <utility>
 #include <unordered_set>
 
 class Algorithm_208992883_322623182_BFS : public MyAlgorithm {
 public:
     Algorithm_208992883_322623182_BFS();
     virtual ~Algorithm_208992883_322623182_BFS();
-	void updateLoc();
-
-    Step nextStep() override;
+    virtual Step nextStep();
 
 private:
-    std::queue<Position> explorationQueue;
+    std::queue<std::pair<int, int>> bfsQueue;
+    std::unordered_set<size_t> visited;
 
-    void enqueueNeighbors(const Position& current);
-    Step translateToStep(const Position& from, const Position& to);
+    void initializeGrid();
+    void updateLoc();
+    bool isAtDockingStation() const;
+    Step handleChargingLogic();
+    bool canMoveTo(const std::pair<int, int>& pos) const;
+    Step getStepTowards(const std::pair<int, int>& pos);
+    Step returnToDocking();
+    void enqueueNeighbors();
+    Step moveTowardsTarget();
 };
 
 #endif // BFS_ALGORITHM_H
